e982bd31182ed9db27548c5c2ac7d93213f91348
asadulla2007 <110012942+asadulla2007@users.noreply.github.com>
2022-08-25 02:24:56 -0400

    Add files via upload

---
+++ Calendar1/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Calendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.get(Calendar.DATE));
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
